7a3df142bdcdbdef4cb570e593610c15c95284fe
yuri <yuricronos@gmail.com>
2024-08-09 11:16:27 +0800

    set update route

--- src/Features/Livewire/SupportLivewireManager.php
+++ src/Features/Livewire/SupportLivewireManager.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 // use Livewire\Features\SupportFileUploads\FilePreviewController;
 // use Livewire\Features\SupportFileUploads\FileUploadController;
 use Livewire\Livewire;
-use Livewire\Mechanisms\HandleRequests\HandleRequests;
+// use Livewire\Mechanisms\HandleRequests\HandleRequests;
 
 class SupportLivewireManager
 {
@@ -17,12 +17,9 @@ class SupportLivewireManager
         }
 
         $updatePath = sprintf("/%s%s", $appRoot, config('lrvlsrvce.livewire_update'));
-        Livewire::setUpdateRoute(
-            fn ($handle) =>
-            Route::post($updatePath, $handle)
-        );
+        Livewire::setUpdateRoute(fn($handle) => Route::post($updatePath, $handle));
 
-        Route::post($updatePath, [HandleRequests::class, 'handleUpdate'])->name('livewire.update');
+        // Route::post($updatePath, [HandleRequests::class, 'handleUpdate'])->name('livewire.update');
         // Route::post('/livewire/upload-file', [FileUploadController::class, 'handle'])->name('livewire.upload-file');
         // Route::get('/livewire/preview-file/{filename}', [FilePreviewController::class, 'handle'])->name('livewire.preview-file');
     }
