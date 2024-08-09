3918a5bba635cfe44ba3da5d99c3de5db87c6570
yuri <yuricronos@gmail.com>
2024-08-09 11:29:12 +0800

    make page command

--- config/lrvlsrvce.php
+++ config/lrvlsrvce.php
@@ -18,6 +18,40 @@ return [
      * third party api url and token 
      */
     'api_url' => env('THIRD_PARTY_API', null),
-    'api_token' => env('THIRD_PARTY_API_TOKEN', null)
+    'api_token' => env('THIRD_PARTY_API_TOKEN', null),
+
+    'page_template' => <<<EOT
+        <x-app-layout>
+
+        <x-slot name="header">
+            <div class="page-header">
+                <h3 class="page-title"> Sample Page </h3>
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="javascript:void(0)"> Home </a></li>
+                        <li class="breadcrumb-item active" aria-current="page"> Sample Page </li>
+                    </ol>
+                </nav>
+            </div>
+        </x-slot>
+
+        <div class="row">
+            <div class="col-lg-12">
+                <x-card>
+                    <x-card-body class="pt-4 pb-5 ps-4 pe-4">
+
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <i> sample page </i>
+                            </div>
+                        </div>
+
+                    </x-card-body>
+                </x-card>
+            </div>
+        </div>
+
+    </x-app-layout>
+    EOT,
 
 ];
---
+++ src/Console/MakePageCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Yuricronos\LaravelService\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+
+class MakePageCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:page {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new page views';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $name = $this->argument('name');
+        $path = resource_path("views/pages/{$name}.blade.php");
+
+        if (File::exists($path)) {
+            $this->error("Page {$name} already exists!");
+            return;
+        }
+
+        File::ensureDirectoryExists(resource_path('views/pages'));
+
+        $template = config('lrvlsrvce.');
+        File::put($path, $template);
+
+        // $stub = <<<EOT
+        //     $template 
+        // EOT;
+        // File::put($path, $stub);
+
+        $this->info("Service {$name} created successfully.");
+    }
+}
